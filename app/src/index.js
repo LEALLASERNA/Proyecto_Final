96315ffdf4adb0ddcb4a3804024a510f22dd18e9
LEALDEV <alejandrolealdev@gmail.com>
2025-01-15 21:28:46 +0100

    Funciona

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,8 @@
-import Web3 from "/web3";
+import Web3 from "web3";
 import MiContrato from "../../build/contracts/miContrato.json";
 
 const dir_contrato = "0x7E0b19B0c376b43d7a7b51D2ddfb26e11c4ebbE1";
-const mi_direccion = "0x1b97d6f688fbe2a6cb76ce31c4b8a7ee573b92a0";
+const mi_direccion = "0x369dad7bee3d9b18222541b1998a9a7c0cd8de22";
 const url = "http://127.0.0.1:8545/";
 
 const web3 = new Web3(url);
@@ -14,12 +14,12 @@ const selector = document.getElementById('tarea');
 const divGuardar = document.getElementById('guardarInfo');
 const divConsultar = document.getElementById('consultarInfo');
 
-selector.addEventListener('change', async() => {
+selector.addEventListener('change', async () => {
   var selector_value = selector.value;
   if (selector_value == "1") {
     divGuardar.style.display = "block";
     divConsultar.style.display = "none";
-  } else if (selector_value == "2"){
+  } else if (selector_value == "2") {
     divGuardar.style.display = "none";
     divConsultar.style.display = "block";
   } else {
@@ -28,17 +28,16 @@ selector.addEventListener('change', async() => {
   }
 });
 
-  btGuardar.addEventListener('click', async() => {
-    var fecha = parseInt(document.getElementById('fecha').value);
-    var precio = parseInt(document.getElementById('precio').value);
-    var destino = document.getElementById('destino').value;
+btGuardar.addEventListener('click', async () => {
+  var fecha = parseInt(document.getElementById('fecha').value);
+  var precio = parseInt(document.getElementById('precio').value);
+  var destino = document.getElementById('destino').value;
+  await contrato.methods.guardar(fecha, destino, precio).send({ from: mi_direccion });
+});
 
-    await contrato.methods.guardar(fecha, destino, precio).send({from: mi_direccion});
-  });
-
-  btConsultar.addEventListener('click', async() => {
-    var fecha = parseInt(document.getElementById('fecha_ver').value);
-    var resultado = await instancia.consultar('resultado');
-    var destino_precio = await contrato.methods.consultar(fecha).call();
-    resultado.innerHTML = "El viaje de " + fecha +" se realizo a " + destino_precio[0] + " y tuvo un costo de " + destino_precio[1] + " euros";
-  });
+btConsultar.addEventListener('click', async () => {
+  var fecha = parseInt(document.getElementById('fecha_ver').value);
+  var resultado = document.getElementById('resultado');
+  var destino_precio = await contrato.methods.consultar(fecha).call();
+  resultado.innerHTML = "El viaje de " + fecha + " se realizo a " + destino_precio[0] + " y tuvo un costo de " + destino_precio[1] + " euros";
+});
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,16 @@
+const ConvertLib = artifacts.require("ConvertLib");
+const MetaCoin = artifacts.require("MetaCoin");
 const MiContrato = artifacts.require("miContrato");
+
+/*
+module.exports = function(deployer) {
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin, miContrato);
+  deployer.deploy(MetaCoin);
+  deployer.deploy(miContrato);
+};
+*/
 
 module.exports = function(deployer) {
   deployer.deploy(MiContrato);
 };
-
